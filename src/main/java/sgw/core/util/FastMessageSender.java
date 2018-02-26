3543ec9c1aeab0d2ecfdadae48c88c8779a90924
Hongji Chen <chjre2015@gmail.com>
2018-02-26 23:44:24 +0800

    add comment on FastMessageSender#send

--- src/main/java/sgw/core/util/FastMessageSender.java
+++ src/main/java/sgw/core/util/FastMessageSender.java
@@ -4,16 +4,29 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
-import org.slf4j.Logger;
 import sgw.core.http_channel.FastMessageToHttpRsp;
 import sgw.core.http_channel.HttpChannelInitializer;
 
 public class FastMessageSender {
 
+    /**
+     * Always invoke {@link sgw.core.http_channel.HttpChannelContext#setSendFastMessage(boolean)}, set to true
+     * when you invoke this method.
+     * @param ctx
+     * @param message {@link FastMessage} generated either because of filters or exceptions caught
+     * @return http channel future
+     */
     public static ChannelFuture send(ChannelHandlerContext ctx, FastMessage message) {
         return send(ctx.channel(), message);
     }
 
+    /**
+     * Always invoke {@link sgw.core.http_channel.HttpChannelContext#setSendFastMessage(boolean)}, set to true
+     * when you invoke this method.
+     * @param channel
+     * @param message {@link FastMessage} generated either because of filters or exceptions caught
+     * @return http channel future
+     */
     public static ChannelFuture send(Channel channel, FastMessage message) {
         ChannelPipeline pipeline = channel.pipeline();
         //modify pipeline
